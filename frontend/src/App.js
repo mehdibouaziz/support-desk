c472b94153d2365ab22e2f8eca8968839d67db5e
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-12-15 20:35:48 +0100

    feat: add home, login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 // components
 import Home from './pages/Home'
@@ -21,6 +23,7 @@ function App() {
         </div>
         
       </Router>
+      <ToastContainer />
     </>
   );
 }
