0d8e3df5a8cdcf6188330ff60fec443fa08b7d9c
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-12-21 03:03:13 +0100

    feat: CRUD functionalities for tickets

--- backend/controlers/ticketControler.js
+++ backend/controlers/ticketControler.js
@@ -8,19 +8,133 @@ const Ticket = require('../models/ticketModel')
 // @route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
+    // get user from the id in the JWT
+    const user = await User.findById(req.user.id)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const tickets = await Ticket.find({user: req.user.id})
     
-    res.status(200).json({message: 'get tickets'})
+    res.status(200).json(tickets)
+})
+
+// @desc Get ONE user ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+    // get user from the id in the JWT
+    const user = await User.findById(req.user.id)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if(!ticket){
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    // check that the user making the request is the owner of the ticket
+    if(ticket.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+    
+    res.status(200).json(ticket)
 })
 
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
+    const {product, description, model} = req.body
+    if(!product || !description || !model) {
+        res.status(400)
+        throw new Error('Please add a product, model and description')
+    }
+
+    // get user from the id in the JWT
+    const user = await User.findById(req.user.id)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // create the ticket
+    const ticket = await Ticket.create({
+        product,
+        description,
+        model,
+        user: req.user.id,
+        status: 'new'
+    })
     
-    res.status(200).json({message: 'new ticket'})
+    res.status(201).json(ticket)
+})
+
+// @desc Delete ticket
+// @route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+    // get user from the id in the JWT
+    const user = await User.findById(req.user.id)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if(!ticket){
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    // check that the user making the request is the owner of the ticket
+    if(ticket.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    await ticket.remove()
+    
+    res.status(200).json({success: true})
+})
+
+// @desc Update ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+    // get user from the id in the JWT
+    const user = await User.findById(req.user.id)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if(!ticket){
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    // check that the user making the request is the owner of the ticket
+    if(ticket.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    const updatedTicket = await Ticket.findByIdAndUpdate(ticket._id.toString(), req.body, {new: true} )
+    
+    res.status(200).json(updatedTicket)
 })
 
 module.exports = {
     getTickets,
-    createTicket
+    getTicket,
+    createTicket,
+    deleteTicket,
+    updateTicket
 }
--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -1,30 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const ticketSchema = mongoose.Schema({
+const ticketSchema = mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     product: {
-        type: String,
-        required: [true, 'Please select a product'],
-        enum: ['iPhone', 'MacBook', 'iPad', 'iMac', 'Watch', 'AirPods']
+      type: String,
+      required: [true, "Please select a product"],
+      enum: ["iPhone", "MacBook", "iPad", "iMac", "Watch", "AirPods"],
+    },
+    model: {
+      type: String,
+      required: [true, "Please specify the model of your device"],
     },
     description: {
-        type: String,
-        required: [true, 'Please type a description of the issue']
+      type: String,
+      required: [true, "Please type a description of the issue"],
     },
     status: {
-        type: String,
-        required: true,
-        enum: ['new', 'open', 'close'],
-        default: 'new'
-    }
-},
-{
+      type: String,
+      required: true,
+      enum: ["new", "open", "close"],
+      default: "new",
+    },
+  },
+  {
     timestamps: true,
-}
-)
+  }
+);
 
-module.exports = mongoose.model('Ticket', ticketSchema)
+module.exports = mongoose.model("Ticket", ticketSchema);
